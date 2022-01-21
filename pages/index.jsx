cfc0db6672e0e8df9fb8148932d0432debcff579
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2022-01-21 23:21:44 +0530

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,63 @@
-import Head from "next/head";
-import { useEffect } from "react";
-import { getPosts } from "../services";
+// import Head from "next/head";
+// import { useEffect } from "react";
+// import { getPosts } from "../services";
+// import { PostCard, Categories, PostWidget } from "../components";
+// import { FeaturedPosts } from "../sections";
+// export default function Home({ posts }) {
+//   return (
+//     <div className="container mx-auto  px-10 mb-8  ">
+//       <Head>
+//         <title>VK BLOG</title>
+//         <link
+//           rel="icon"
+//           href="https://raw.githubusercontent.com/shanuv000/shanuv000/main/dear.png"
+//         />
+//       </Head>
+//       <FeaturedPosts />
+//       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+//         <div className="lg:col-span-8 col-span-1 ">
+//           {posts.map((post, i) => (
+//             <PostCard post={post.node} key={i} />
+//           ))}
+//         </div>
+
+//         <div className="lg:col-span-4 col-span-1">
+//           <div className="lg:sticky relative top-8">
+//             <PostWidget />
+//             <Categories />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// // Fetch data at build time
+// export async function getStaticProps() {
+//   const posts = (await getPosts()) || [];
+//   return {
+//     props: { posts },
+//   };
+// }
+
+// // export async function getStaticProps() {
+// //   const posts = (await getPosts()) || [];
+// //   return { props: { posts } };
+// // }
+import { FeaturedPosts } from "../sections/index";
 import { PostCard, Categories, PostWidget } from "../components";
-import { FeaturedPosts } from "../sections";
+import { getPosts } from "../services";
+
 export default function Home({ posts }) {
   return (
-    <div className="container mx-auto  px-10 mb-8  ">
-      <Head>
-        <title>VK BLOG</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1 ">
-          {posts.map((post, i) => (
-            <PostCard post={post.node} key={i} />
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
-
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
@@ -36,8 +76,3 @@ export async function getStaticProps() {
     props: { posts },
   };
 }
-
-// export async function getStaticProps() {
-//   const posts = (await getPosts()) || [];
-//   return { props: { posts } };
-// }
