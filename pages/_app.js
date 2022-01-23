127dabad17262db2096f3db34d413171fd07db7d
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2022-01-23 06:40:19 +0530

    once moreseasjdnosasasdakssakdmsssssaasasdshsss3

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,16 @@ import React from "react";
 
 import "../styles/globals.scss";
 import { Layout } from "../components";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+// import Document, { Html, Head, Main, NextScript } from "next/document";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>{"VK BLOG"}</title>
+        <link rel="icon" href="/dear.png" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
