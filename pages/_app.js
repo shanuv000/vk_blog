b5b5a0064a0a59dc7e5cc08e742a2019e5304f8d
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2022-01-30 09:27:30 +0530

    shajmlsdjhjs

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,46 @@ import React from "react";
 
 import "../styles/globals.scss";
 import { Layout } from "../components";
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 // import Document, { Html, Head, Main, NextScript } from "next/document";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return (
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (<> {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     <Layout>
-      <Head>
+      {/* <Head>
         <title>{"key2success"}</title>
         <link rel="icon" href="/logo10.svg" />
         <script
@@ -16,9 +49,9 @@ function MyApp({ Component, pageProps }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5634941748977646"
           crossorigin="anonymous"
         ></script>
-      </Head>
+      </Head> */}
       <Component {...pageProps} />
-    </Layout>
+    </Layout></>
   );
 }
 
