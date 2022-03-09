4b29a94016dc30316af911907007926023dc336a
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2022-03-10 00:33:14 +0530

    asjdbajd jabdkas

--- components/Navbar_post_details.jsx
+++ components/Navbar_post_details.jsx
@@ -31,7 +31,14 @@ const Navbar_post_details = ({ post }) => {
               class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
             >
               <img src="/message.png" width={30} alt="" />
-            </a> */}
+            </a> */}{" "}
+      <a
+        href={`https://wa.me/?text=https://www.keytosuccess.me/post/${slugs}`}
+        data-action="share/whatsapp/share"
+        class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 "
+      >
+        <LazyLoadImage src="/whatsapp2.svg" width={45} alt="" />
+      </a>
       <a
         target={"_blank"}
         href={`https://www.linkedin.com/sharing/share-offsite/?url=https://www.keytosuccess.me/post/${post.slug}
@@ -45,26 +52,19 @@ const Navbar_post_details = ({ post }) => {
           // className="invisible  md:visible"
         />
       </a>
-      <a
+      {/* <a
         target={"_blank"}
         href={`https://www.tumblr.com/widgets/share/tool?canonicalUrl=https://www.keytosuccess.me/post/${post.slug}&title=${post.title}&caption=${post.excerpt}&tags={#key2success}`}
         class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 hidden lg:inline-block "
       >
         <LazyLoadImage src="/tumblr.svg" width={45} alt="" />
-      </a>
+      </a> */}
       <a
         target={"_blank"}
         href={`http://pinterest.com/pin/create/button/?url=https://www.keytosuccess.me/post/${post.slug}`}
         class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 hidden lg:inline-block "
       >
         <LazyLoadImage src="/pin.svg" width={45} alt="" />
-      </a>
-      <a
-        href={`https://wa.me/?text=https://www.keytosuccess.me/post/${slugs}`}
-        data-action="share/whatsapp/share"
-        class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900 lg:hidden"
-      >
-        <LazyLoadImage src="/whatsapp2.svg" width={45} alt="" />
       </a>
       <a
         href={`https://t.me/share/url?url=https://www.keytosuccess.me/post/${slugs}&text=${post.title}`}
