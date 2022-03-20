7d1017da4dca1fb214a6a809fe4d31f8b9c86406
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2022-03-20 11:10:10 +0530

    asdasudbsj

--- components/Insatgarm.jsx
+++ components/Insatgarm.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import InstagramEmbed from "react-instagram-embed";
 
 const Insatgarm = () => {
+  const clientAccessToken = "1038213280237143|32417a123db39472d95eee30f37d9001";
   return (
     <>
       <h1>Shanu</h1>
       <InstagramEmbed
         url="https://instagr.am/p/Zw9o4/"
-        clientAccessToken="1038213280237143|IGQVJWTEd2c3RmYzlLVjdSdUdEMkFGMDdyVEJsc05rdWcxN045WHJvOFV3REtfTzdsUTJkeDF1TTRKODctajBIbzhwZA3JIMWRCMUMwNktwOHdrczFGWmtfbWhkbWhRamdXeTlLVDFXZATV5YzJjWGpPVwZDZD"
+        clientAccessToken={clientAccessToken}
         maxWidth={320}
-        minHeight={800}
+        height={800}
         hideCaption={false}
         containerTagName="div"
+        protocol=""
+        injectScript
+        onLoading={() => {}}
+        onSuccess={() => {}}
+        onAfterRender={() => {}}
+        onFailure={() => {}}
       />
     </>
   );
