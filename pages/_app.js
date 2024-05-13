fd3f047b7cb71ad051d06b0d430a089a89209fcf
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-05-13 15:33:39 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -23,26 +23,26 @@ function MyApp({ Component, pageProps }) {
           href="https://firebasestorage.googleapis.com/v0/b/shanu-chess.appspot.com/o/logo%2Fblog-logo%2Fmain-logo-transparent.png?alt=media&token=94294bb8-3404-44ce-81ab-b2f36ae1988e"
         />
         {/* SEO Meta Tags */}
-        <meta
+        {/* <meta
           name="description"
           content="Explore a world of diverse content on [Your Blogging Website Name]. From the latest in tech innovations to captivating entertainment news and thrilling sports updates, our platform offers a plethora of engaging articles and insights. Delve into a universe of information and entertainment, all in one place."
-        />
+        /> */}
         <meta
           name="keywords"
           content="tech, entertainment, sports, articles, news, updates, reviews, analysis, blog, blogging, diverse content, information, insights"
         />
-        <meta property="og:title" content="key2success" />
-        <meta
+        {/* <meta property="og:title" content="key2success" /> */}
+        {/* <meta
           property="og:description"
           content="Discover a treasure trove of content on [Your Blogging Website Name]. From the latest in tech to captivating entertainment and thrilling sports updates, explore a world of diverse articles and insights, all in one place."
-        />
-        <meta
+        /> */}
+        {/* <meta
           property="og:image"
           content="https://firebasestorage.googleapis.com/v0/b/shanu-chess.appspot.com/o/Portfolio%20work%2Fs.png?alt=media&token=ea44c393-6f7d-470c-9750-a707100affb1"
-        />
-        <meta property="og:url" content="https://onlyblog.vercel.app" />
+        /> */}
+        {/* <meta property="og:url" content="https://onlyblog.vercel.app" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="key2success" />
+        <meta name="twitter:title" content="key2success" /> */}
       </Head>
       <Layout>
         <Component {...pageProps} /> <GoogleAnalytics gaId="G-LW10VJQH6L" />
