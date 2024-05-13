b90de55e1102e29b05de62e456d9115309feed03
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-05-13 14:06:55 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,54 +2,46 @@ import React from "react";
 
 import "../styles/globals.scss";
 import { Layout } from "../components";
-import { useEffect } from "react";
-import Script from "next/script";
+// import { useEffect } from "react";
+// import Script from "next/script";
 import { useRouter } from "next/router";
 // import * as gtag from "../lib/gtag";
 // import Document, { Html, Head, Main, NextScript } from "next/document";
-// import Head from "next/head";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  // useEffect(() => {
-  //   console.log("shanu");
-  //   const handleRouteChange = (url) => {
-
-  //   };
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
 
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      {/* <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      /> */}
-      <title>{"key2success"}</title>
-      <link rel="icon" href="/logo10.svg" />
-      {/* <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5634941748977646"
-        crossorigin="anonymous"
-      ></script> */}
+      <Head>
+        <title>Only Blog</title> {"blog"}
+        <link
+          rel="icon"
+          href="https://firebasestorage.googleapis.com/v0/b/shanu-chess.appspot.com/o/logo%2Fblog-logo%2Fmain-logo-transparent.png?alt=media&token=94294bb8-3404-44ce-81ab-b2f36ae1988e"
+        />
+        {/* SEO Meta Tags */}
+        <meta
+          name="description"
+          content="Explore a world of diverse content on [Your Blogging Website Name]. From the latest in tech innovations to captivating entertainment news and thrilling sports updates, our platform offers a plethora of engaging articles and insights. Delve into a universe of information and entertainment, all in one place."
+        />
+        <meta
+          name="keywords"
+          content="tech, entertainment, sports, articles, news, updates, reviews, analysis, blog, blogging, diverse content, information, insights"
+        />
+        <meta property="og:title" content="key2success" />
+        <meta
+          property="og:description"
+          content="Discover a treasure trove of content on [Your Blogging Website Name]. From the latest in tech to captivating entertainment and thrilling sports updates, explore a world of diverse articles and insights, all in one place."
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/shanu-chess.appspot.com/o/Portfolio%20work%2Fs.png?alt=media&token=ea44c393-6f7d-470c-9750-a707100affb1"
+        />
+        <meta property="og:url" content="https://onlyblog.vercel.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="key2success" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
