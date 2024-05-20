1a58c5dba320061fb9f337321ff9becc1c2ddc77
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-05-20 12:36:12 +0530

    up

--- components/Nested_Table.jsx
+++ components/Nested_Table.jsx
@@ -1,24 +1,40 @@
 import React from "react";
+
 const SimpleTable = ({ data }) => {
   // Check if the table has a table_body
   const tableBody = data.children.find((child) => child.type === "table_body");
 
+  // Extract rows data
+  const rows = tableBody ? tableBody.children : data.children;
+
   return (
-    <table className="border-collapse border border-slate-400 w-full mb-4 ">
+    <table className="border-collapse border-none w-full mb-4">
       <tbody>
-        {/* If table_body exists, use its children, otherwise use table's children directly */}
-        {(tableBody ? tableBody.children : data.children).map(
-          (rowData, rowIndex) => (
+        {rows.map((rowData, rowIndex) =>
+          rowIndex === 0 ? (
             <tr
               key={rowIndex}
-              className="border  border-black font-medium text-center"
+              className="border border-black font-medium text-center"
             >
               {rowData.children.map((cellData, cellIndex) => (
-                <td key={cellIndex} className="border  border-black p-2 bold">
-                  {/* Handle the paragraph within the cell */}
+                <td key={cellIndex} className="border border-black p-2">
                   {cellData.children[0].children.map(
                     (paragraphChild, pIndex) => (
-                      <p key={pIndex} className="text-black">
+                      <p key={pIndex} className="text-red-400 font-semibold">
+                        {paragraphChild.text}
+                      </p>
+                    )
+                  )}
+                </td>
+              ))}
+            </tr>
+          ) : (
+            <tr key={rowIndex} className="border border-black font-medium ">
+              {rowData.children.map((cellData, cellIndex) => (
+                <td key={cellIndex} className="border border-black p-2">
+                  {cellData.children[0].children.map(
+                    (paragraphChild, pIndex) => (
+                      <p key={pIndex} className="text-gray-600 font-medium">
                         {paragraphChild.text}
                       </p>
                     )
