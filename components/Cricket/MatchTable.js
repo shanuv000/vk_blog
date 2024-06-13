0805a6f7c5949827d26d0d57e6d3d43b1f2ab760
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-06-13 12:32:56 +0530

    up

--- components/Cricket/MatchTable.js
+++ components/Cricket/MatchTable.js
@@ -7,7 +7,7 @@ const ScheduleTable = () => {
 
   if (loadingSchedule) {
     return (
-      <div>
+      <div className="flex justify-center items-center h-full">
         <BarLoader color="#FFFFFF" />
       </div>
     );
@@ -21,7 +21,7 @@ const ScheduleTable = () => {
     Canada: "🇨🇦",
     Pakistan: "🇵🇰",
     Ireland: "🇮🇪",
-    Scotland: "🏴󠁧󠁢󠁳󠁣󠁴󠁿",
+    Scotland: "🏴",
     Australia: "🇦🇺",
     Namibia: "🇳🇦",
     England: "🇬🇧",
@@ -38,45 +38,66 @@ const ScheduleTable = () => {
     "Sri Lanka": "🇱🇰",
   };
 
+  const getSeriesFormClass = (form) => {
+    if (form === "W") return "bg-green-500 text-white";
+    if (form === "L") return "bg-red-500 text-white";
+    if (form === "NR") return "bg-gray-500 text-white";
+    if (form === "T") return "bg-yellow-500 text-white"; // Adding "T" for ties
+    return "bg-gray-500 text-white";
+  };
+
+  const parseSeriesForm = (seriesForm) => {
+    const forms = [];
+    for (let i = 0; i < seriesForm.length; i++) {
+      if (seriesForm[i] === "N" && seriesForm[i + 1] === "R") {
+        forms.push("NR");
+        i++; // Skip next character as it's part of 'NR'
+      } else {
+        forms.push(seriesForm[i]);
+      }
+    }
+    return forms;
+  };
+
   return (
     <div className="p-2 md:p-4">
       {schedule.map((group) => (
-        <div key={group.groupName} className="mb-8">
-          <h2 className="text-lg md:text-2xl font-semibold mb-4 text-primary text-orange-300 text-center">
+        <div key={group.groupName} className="mb-4 md:mb-8">
+          <h2 className="text-base md:text-xl font-semibold mb-2 md:mb-4 text-primary text-orange-300 text-center">
             {group.groupName}
           </h2>
           <div className="overflow-x-auto rounded">
-            <table className="min-w-full bg-white">
+            <table className="min-w-full bg-white text-xs md:text-sm">
               <thead>
                 <tr>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-left">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-left">
                     Team
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Matches
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Wins
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Losses
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Ties
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     No Result
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Points
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Net Run Rate
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Series Form
                   </th>
-                  <th className="py-2 px-2 md:px-4 bg-gray-200 text-right">
+                  <th className="py-1 px-2 md:px-4 bg-gray-200 text-right">
                     Next Match
                   </th>
                 </tr>
@@ -93,34 +114,45 @@ const ScheduleTable = () => {
                         : "bg-white"
                     }
                   >
-                    <td className="py-2 px-2 md:px-4 border-b">
+                    <td className="py-1 px-2 md:px-4 border-b">
                       {flagEmojis[team.team]} {team.team}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.matches}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.wins}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.losses}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.ties}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.noResult}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.points}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.netRunRate}
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
-                      {team.seriesForm}
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
+                      <div className="inline-flex flex-wrap gap-0.5 md:gap-1">
+                        {parseSeriesForm(team.seriesForm).map((form, index) => (
+                          <span
+                            key={index}
+                            className={`inline-block px-1 md:px-2 py-0.5 md:py-1 rounded ${getSeriesFormClass(
+                              form
+                            )}`}
+                          >
+                            {form}
+                          </span>
+                        ))}
+                      </div>
                     </td>
-                    <td className="py-2 px-2 md:px-4 border-b text-right">
+                    <td className="py-1 px-2 md:px-4 border-b text-right">
                       {team.nextMatch.nextMatches}
                     </td>
                   </tr>
