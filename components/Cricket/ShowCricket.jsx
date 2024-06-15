25e6b9e64da8c0f6e149265761216eecfa6eb0ce
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-06-15 11:37:58 +0530

    up

--- components/Cricket/ShowCricket.jsx
+++ components/Cricket/ShowCricket.jsx
@@ -23,8 +23,9 @@ const TabPanel = ({ children, value, index }) => {
 const ShowCricket = () => {
   const { isLiveScore } = useData();
 
-  const settingDefaultTab = () => (isLiveScore ? "live" : "recent");
-  const [selectedTab, setSelectedTab] = useState(settingDefaultTab);
+  const [selectedTab, setSelectedTab] = useState(
+    isLiveScore ? "live" : "recent"
+  );
 
   const handleChange = (event, newValue) => {
     setSelectedTab(newValue);
@@ -32,46 +33,48 @@ const ShowCricket = () => {
 
   return (
     <>
-      <Tabs
-        value={selectedTab}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="match tabs"
-        className="bg-white border-b border-gray-300 w-full py-2 sm:py-1 px-2 sm:px-4 rounded"
-        style={{ overflowX: "auto" }} // Ensure overflow is handled
-      >
-        {isLiveScore && (
+      <Box display="flex" justifyContent="center">
+        <Tabs
+          value={selectedTab}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="match tabs"
+          className="bg-white border-b border-gray-300 w-full py-2 sm:py-1 px-2 sm:px-4 rounded"
+          style={{ overflowX: "auto" }} // Ensure overflow is handled
+        >
+          {isLiveScore && (
+            <Tab
+              className={`px-4 py-2 m-1 ${
+                selectedTab === "live" ? "text-white" : "text-gray-600"
+              } rounded-lg transition duration-200`}
+              label="Live Match"
+              value="live"
+            />
+          )}
           <Tab
             className={`px-4 py-2 m-1 ${
-              selectedTab === "live" ? "text-white" : "text-gray-600"
+              selectedTab === "recent" ? "text-white " : "text-gray-600"
             } rounded-lg transition duration-200`}
-            label="Live Match"
-            value="live"
+            label="Recent Match"
+            value="recent"
           />
-        )}
-        <Tab
-          className={`px-4 py-2 m-1 ${
-            selectedTab === "recent" ? "text-white" : "text-gray-600"
-          } rounded-lg transition duration-200`}
-          label="Recent Match"
-          value="recent"
-        />
-        <Tab
-          className={`px-4 py-2 m-1 ${
-            selectedTab === "upcoming" ? "text-white" : "text-gray-600"
-          } rounded-lg transition duration-200`}
-          label="Upcoming Match"
-          value="upcoming"
-        />
-        <Tab
-          className={`px-4 py-2 m-1 ${
-            selectedTab === "table" ? "text-white" : "text-gray-600"
-          } rounded-lg transition duration-200`}
-          label="T20 World Cup Rankings"
-          value="table"
-        />
-      </Tabs>
+          <Tab
+            className={`px-4 py-2 m-1 ${
+              selectedTab === "upcoming" ? "text-white " : "text-gray-600"
+            } rounded-lg transition duration-200`}
+            label="Upcoming Match"
+            value="upcoming"
+          />
+          <Tab
+            className={`px-4 py-2 m-1 ${
+              selectedTab === "table" ? "text-white " : "text-gray-600"
+            } rounded-lg transition duration-200`}
+            label="T20 World Cup Rankings"
+            value="table"
+          />
+        </Tabs>
+      </Box>
       <Box className="w-full mt-4 p-4">
         {isLiveScore && (
           <TabPanel value={selectedTab} index="live">
