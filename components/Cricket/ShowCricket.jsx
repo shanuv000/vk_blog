62bfe7e18678e317caf18e57b462af42889fd65a
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-07-26 01:49:22 +0530

    update

--- components/Cricket/ShowCricket.jsx
+++ components/Cricket/ShowCricket.jsx
@@ -8,32 +8,39 @@ import { useData } from "../../store/HandleApiContext";
 import useSound from "use-sound";
 import tabChangeSound from "../../public/cricket/sport.mp3"; // Ensure this path is correct
 
-const TabPanel = ({ children, value, index }) => {
-  return (
-    <Slide
-      direction="left"
-      in={value === index}
-      mountOnEnter
-      unmountOnExit
-      timeout={500}
-    >
-      <div>{children}</div>
-    </Slide>
-  );
-};
+const TabPanel = ({ children, value, index }) => (
+  <Slide
+    direction="left"
+    in={value === index}
+    mountOnEnter
+    unmountOnExit
+    timeout={500}
+  >
+    <div>{children}</div>
+  </Slide>
+);
 
 const ShowCricket = () => {
   const { isLiveScore } = useData();
   const [selectedTab, setSelectedTab] = useState(
     isLiveScore ? "live" : "recent"
   );
-
   const [play] = useSound(tabChangeSound);
 
   const handleChange = (event, newValue) => {
     setSelectedTab(newValue);
     play(); // Play sound on tab change
   };
+
+  const renderTab = (label, value) => (
+    <Tab
+      className={`px-4 py-2 m-1 ${
+        selectedTab === value ? "text-white" : "text-gray-600"
+      } rounded-lg transition duration-200`}
+      label={label}
+      value={value}
+    />
+  );
 
   return (
     <>
@@ -47,36 +54,10 @@ const ShowCricket = () => {
           className="bg-white border-b border-gray-300 w-full py-2 sm:py-1 px-2 sm:px-4 rounded"
           style={{ overflowX: "auto" }}
         >
-          {isLiveScore && (
-            <Tab
-              className={`px-4 py-2 m-1 ${
-                selectedTab === "live" ? "text-white " : "text-gray-600"
-              } rounded-lg transition duration-200`}
-              label="Live Match"
-              value="live"
-            />
-          )}
-          <Tab
-            className={`px-4 py-2 m-1 ${
-              selectedTab === "recent" ? "text-white " : "text-gray-600"
-            } rounded-lg transition duration-200`}
-            label="Recent Match"
-            value="recent"
-          />
-          <Tab
-            className={`px-4 py-2 m-1 ${
-              selectedTab === "upcoming" ? "text-white " : "text-gray-600"
-            } rounded-lg transition duration-200`}
-            label="Upcoming Match"
-            value="upcoming"
-          />
-          <Tab
-            className={`px-4 py-2 m-1 ${
-              selectedTab === "table" ? "text-white " : "text-gray-600"
-            } rounded-lg transition duration-200`}
-            label="T20 World Cup Rankings"
-            value="table"
-          />
+          {isLiveScore && renderTab("Live Match", "live")}
+          {renderTab("Recent Match", "recent")}
+          {renderTab("Upcoming Match", "upcoming")}
+          {renderTab("T20 World Cup Rankings", "table")}
         </Tabs>
       </Box>
       <Box className="w-full mt-4 p-4">
